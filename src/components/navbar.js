09818be2ac9a0c6efd595e44095e237f8ee4cffc
tRaihanSarkar <t.raihansarkar@gmail.com>
2023-06-12 11:37:44 +0600

    Added search icon and login icon for responsive design and partially fixed navbar issues

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,18 +9,30 @@ export default function Navbar() {
     return (
         <nav>
             <img src={process.env.PUBLIC_URL + "img/snlogo.png"} alt="Service Nin logo" className="nav--img" onClick={handleClick}/>
+            {/* Adding Signup/login and search icon */}
+            <div className="nav--icons">
+                <a href="#" className="search--ico">
+                    <img src={process.env.PUBLIC_URL + "img/search-ico.png"} alt="Search Icon" className="search--img" onClick={handleClick}/>
+                </a>
+                <a href="#" className="login--ico">
+                    <img src={process.env.PUBLIC_URL + "img/login-ico.png"} alt="login Icon" className="login--img" onClick={handleClick}/>
+                </a>
+            </div>
+            
+            
+            
             {/* Adding hamburger menu */}
-            <a href="#" className="toggle-button">
+            <a href="#" className="toggle-button" onClick={() => setOpen(!open)}>
                 <span className="bar"></span>
                 <span className="bar"></span>
                 <span className="bar"></span>
             </a>
             
-            <ul className="nav--links">
-                <li><a href="" onClick={() => setOpen(!open)}>All Services</a></li>
-                <li><a href="">Earn With Us</a></li>
-                <li><a href="">Track order</a></li>
-                <li><a href="">Contact</a></li>
+            <ul className={` ${open? "nav--links":"nav--closed"}`}>
+                <li><a href="#">All Services</a></li>
+                <li><a href="#">Earn With Us</a></li>
+                <li><a href="#">Track order</a></li>
+                <li><a href="#">Contact</a></li>
             </ul>
 
             <div className=".login">
