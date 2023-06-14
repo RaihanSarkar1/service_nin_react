1f84fe2402ec97006780e11f6f0dfdddb0b4a147
tRaihanSarkar <t.raihansarkar@gmail.com>
2023-06-14 20:23:58 +0600

    Removed Image for bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Navbar from './components/navbar';
-import Carousel from './components/carousel';
+
 
 
 function App() {
   return (
     <>
       <Navbar />
-      <Carousel />
+      
     </>
   );
 }
