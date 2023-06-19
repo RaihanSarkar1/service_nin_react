2f837662f6860952f9ad5f80cae3c3ef52c19648
tRaihanSarkar <t.raihansarkar@gmail.com>
2023-06-19 23:29:51 +0600

    Added search and location button to hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,8 +2,19 @@
 export default function Hero() {
     return (
         <>
-            <div className="img">
-                sdtgds
+            <div className="hero">
+                
+                <div className="location">
+                    <button className="btn-loc">
+                        <span>
+                            <img src={process.env.PUBLIC_URL + "img/location-ico.png"}/>
+                        </span>
+                        <span className="btn-loc-txt">Uttara</span>
+                    </button>
+                </div>
+                
+                <input className="hero-search" type="text" placeholder="Search.."></input>
+
             </div>
         </>
     )
