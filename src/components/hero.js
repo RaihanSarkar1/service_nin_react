8319bf64be828c659062516928ce577535af9ad6
Raihan Sarkar <t.raihansarkar@gmail.com>
2023-06-21 21:46:40 +0600

    Added dialog modal for location and styled it using CSS

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,15 +1,35 @@
 
 export default function Hero() {
+    
+    const modal = document.querySelector('dialog');
+    function handleLocClick() {
+        modal.showModal();
+
+    }
+
+    // Close modal on clicking outside
+    modal.addEventListener("click", e => {
+        const dialogDimensions = modal.getBoundingClientRect()
+        if (
+          e.clientX < dialogDimensions.left ||
+          e.clientX > dialogDimensions.right ||
+          e.clientY < dialogDimensions.top ||
+          e.clientY > dialogDimensions.bottom
+        ) {
+          modal.close()
+        }
+      })
+
     return (
         <>
             <div className="hero">
                 
                 <div className="location">
-                    <button className="btn-loc">
+                    <button className="btn-loc" onClick={() => {handleLocClick()}}>
                         <span>
                             <img src={process.env.PUBLIC_URL + "img/location-ico.png"}/>
                         </span>
-                        <span className="btn-loc-txt">Uttara</span>
+                        <span className="btn-loc-txt">Mohammadpur</span>
                     </button>
                 </div>
                 <div className="hero-serach-sect">
@@ -21,6 +41,17 @@ export default function Hero() {
                 
 
             </div>
+
+            <dialog className="modal">
+                <div className="modal-title">
+                    <img src={process.env.PUBLIC_URL + "img/modal-loc.png"} />
+                    <span className="modal-text">Select your Area in Dhaka</span>
+                </div>
+                <div className="modal-body">
+                    <input className="modal-search"></input>
+                    <button className="modal-button">Current Location</button>
+                </div>
+            </dialog>
         </>
     )
 }
