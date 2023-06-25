90ad1e3cd87c108859001966e8d577dfb14c877f
tRaihanSarkar <t.raihansarkar@gmail.com>
2023-06-25 20:46:55 +0600

    Use the useRef hook to access the DOM element modal to resolve modal selection error

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,57 +1,47 @@
+import { useRef } from "react"
 
 export default function Hero() {
-    
-    const modal = document.querySelector('dialog');
+
+    const modalRef = useRef(null);
+
     function handleLocClick() {
-        modal.showModal();
+        modalRef.current.showModal();
 
     }
-
-    // Close modal on clicking outside
-    modal.addEventListener("click", e => {
-        const dialogDimensions = modal.getBoundingClientRect()
-        if (
-          e.clientX < dialogDimensions.left ||
-          e.clientX > dialogDimensions.right ||
-          e.clientY < dialogDimensions.top ||
-          e.clientY > dialogDimensions.bottom
-        ) {
-          modal.close()
-        }
-      })
+        
 
     return (
         <>
-            <div className="hero">
-                
-                <div className="location">
-                    <button className="btn-loc" onClick={() => {handleLocClick()}}>
-                        <span>
-                            <img src={process.env.PUBLIC_URL + "img/location-ico.png"}/>
-                        </span>
-                        <span className="btn-loc-txt">Mohammadpur</span>
-                    </button>
-                </div>
-                <div className="hero-serach-sect">
-                    <input className="hero-search" type="text" placeholder="Search.."></input>
-                    <button className="btn-hero-search">
-                        <img src={process.env.PUBLIC_URL + "img/btn-search-ico.png"}/>
-                    </button>
-                </div>
-                
-
+        <div className="hero">
+            
+            <div className="location" onClick={() => handleLocClick()}>
+                <button className="btn-loc">
+                    <span>
+                        <img src={process.env.PUBLIC_URL + "img/location-ico.png"}/>
+                    </span>
+                    <span className="btn-loc-txt">Mohammadpur</span>
+                </button>
             </div>
+            <div className="hero-serach-sect">
+                <input className="hero-search" type="text" placeholder="Search.."></input>
+                <button className="btn-hero-search">
+                    <img src={process.env.PUBLIC_URL + "img/btn-search-ico.png"}/>
+                </button>
+            </div>
+            
 
-            <dialog className="modal">
-                <div className="modal-title">
-                    <img src={process.env.PUBLIC_URL + "img/modal-loc.png"} />
-                    <span className="modal-text">Select your Area in Dhaka</span>
-                </div>
-                <div className="modal-body">
-                    <input className="modal-search"></input>
-                    <button className="modal-button">Current Location</button>
-                </div>
-            </dialog>
+        </div>
+
+        <dialog className="modal" ref={modalRef}>
+            <div className="modal-title">
+                <img src={process.env.PUBLIC_URL + "img/modal-loc.png"} />
+                <span className="modal-text">Select your Area in Dhaka</span>
+            </div>
+            <div className="modal-body">
+                <input className="modal-search"></input>
+                <button className="modal-button">Current Location</button>
+            </div>
+        </dialog>
         </>
     )
 }
