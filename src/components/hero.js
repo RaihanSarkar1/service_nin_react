b200d20c7da5209961cc1f5725e74323a9a47428
tRaihanSarkar <t.raihansarkar@gmail.com>
2023-06-25 22:48:44 +0600

    1. Added Locations in modal and used react useState to store location. 2. used onChange=filter to detect changes in input event and update the respective state. The modal closes after selecting the correct state

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,8 +1,41 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 export default function Hero() {
 
     const modalRef = useRef(null);
+
+    // Creating a list of locations
+    const locations = ['Hatirjheel Lake', 'Abdullahpur', 'Uttara', 'Mirpur', 'Pallabi', 'Kazipara', 'Kafrul', 'Agargaon', 'Sher-e-Bangla Nagar', 'Cantonment area', 'Banani', 'Gulshan', 'Mohakhali', 'Bashundhara R/A', 'Banasree', 'Baridhara', 'Uttarkhan', 'Dakshinkhan', 'Bawnia', 'Khilkhet', 'Tejgaon', 'Farmgate', 'Mohammadpur', 'Rampura', 'Badda', 'Satarkul', 'Beraid', 'Khilgaon', 'Vatara', 'Gabtali']
+
+    // Creating a found state or result of the search app
+    const [loc, setLoc] = useState('Bashundhara');
+
+    // Creating a state that shows the search result locations
+    const [foundLoc, setFoundLoc] = useState(locations);
+
+    // When a location is clicked in the modal update loc & close modal
+    function handleLocsClick(e) {
+        console.log(e);
+        setLoc(e);
+        modalRef.current.close()
+    }
+
+    // filtering list after string is typed in input
+    const filter = (e) => {
+        // getting the value of the input field using event
+        const searchword = e.target.value;
+
+        if (searchword !== '') {
+            const result = locations.filter((location) => {
+                return location.toLowerCase().startsWith(searchword.toLowerCase());
+            });
+            setFoundLoc(result);
+        } else {
+            setFoundLoc(locations);
+        }
+    }
+
+
 
     function handleLocClick() {
         modalRef.current.showModal();
@@ -30,7 +63,7 @@ export default function Hero() {
                     <span>
                         <img src={process.env.PUBLIC_URL + "img/location-ico.png"}/>
                     </span>
-                    <span className="btn-loc-txt">Mohammadpur</span>
+                    <span className="btn-loc-txt">{loc}</span>
                 </button>
             </div>
             <div className="hero-serach-sect">
@@ -49,8 +82,16 @@ export default function Hero() {
                 <span className="modal-text">Select your Area in Dhaka</span>
             </div>
             <div className="modal-body">
-                <input className="modal-search"></input>
+                <input className="modal-search" onChange={filter}></input>
                 <button className="modal-button">Current Location</button>
+
+                <div className="locations">
+                    {foundLoc.map((location, index) => (
+                        <li key={index} onClick={() => {handleLocsClick(location)}}>
+                            <span className="location" >{location}</span>
+                        </li>
+                    ))}
+                </div>
             </div>
         </dialog>
         </>
